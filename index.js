ad846f512dbe72908487e91a86f350016dc652ee
Borissimus <borysworking@gmail.com>
2022-07-15 00:58:30 +0300

    Login page runs

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
 var http = require('http'); // 1 - Import Node.js core module
 var fs = require('fs');
+var path = require('path');
+var express = require('express');
+app = express();
+
+var login_html = "./login_page/login.html";
+
+var dirPath = path.join(__dirname, 'login_page');
+var dirJsLib = path.join(__dirname, 'js_lib');
+
+app.use('/js_lib', express.static(dirJsLib));
+app.use(express.static(dirPath));
+
+function loginPageReq(req, res) {   // 2 - creating server
+    var server = app.listen(80, function () {
+        var host = 'localhost';
+        var port = server.address().port;
+        console.log('listening on http://'+host+':'+port+'/');
+    });
+}
+
+loginPageReq(null, null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var http = require('http'); // 1 - Import Node.js core module
+var fs = require('fs');
 var login_html = "./login_page/login.html";
 
 function loginPageReq(req, res) {   // 2 - creating server
@@ -19,4 +62,4 @@ function loginPageReq(req, res) {   // 2 - creating server
 }
 
 http.createServer(loginPageReq).listen(80);
-
+*/
